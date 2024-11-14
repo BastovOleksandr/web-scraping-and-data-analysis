b0acf121d3575ff7e22cc45fc3c8cda26023a310
Olexandr Bastov <iamlucky1990@gmail.com>
2024-11-14 23:50:05 +0200

    add prepared 'analysis_config.py'

---
+++ analysis_config.py
@@ -0,0 +1,10 @@
+# All other dirs will be saved inside this one
+BASE_PLOT_DIR = "plots"
+
+# Name of directories for all plot types separately
+TECH_POPULARITY_DIR = "tech_popularity"
+EXPERIENCE_POPULARITY_DIR = "exp_popularity"
+EXPERIENCE_REMOTE_DIR = "exp_remote"
+
+# The image format in which a diagrams will be saved, recognized by matplotlib
+IMAGE_FORMAT = ".png"
